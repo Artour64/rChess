2577fad561d84df117a3f165ac5f8d397d638b80
Artour Konstantinov <artoukons@gmail.com>
2023-05-23 23:13:57 -0400

    pawn and knight moves

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::{
 
 fn main() {
 	let mut game = GameManager::console_game();
-	
 	game.run();
 
+	//game.game.get_moves_cord(CORD_NONE);
 	//let game = Game::new();
     //println!("{}", game);
     
@@ -88,6 +88,77 @@ impl Square {
 			}
 		}
 	}
+	
+	//useless function? remove?
+	fn compareSquaresColor(s1: &Square, s2: &Square) -> SquareComparison {
+		match s1 {
+			Empty => SquareComparison::Empty,
+			Square::Piece(piece1) => match s2 {
+				Empty => SquareComparison::Empty,
+				Square::Piece(piece2) => {
+					if piece1.color == piece2.color {
+						SquareComparison::ColorSame
+					} else {
+						SquareComparison::ColorOther
+					}
+				}
+			}
+		}
+	}
+	
+	fn compareColor(&self, color: bool) -> SquareComparison {
+		match self {
+			Empty => SquareComparison::Empty,
+			Square::Piece(Piece{color:piece_color, ..}) => {
+				if color == *piece_color {
+					SquareComparison::ColorSame
+				} else {
+					SquareComparison::ColorOther
+				}
+			}
+		}
+	}
+}
+
+enum SquareComparison {
+	Empty,
+	ColorSame,
+	ColorOther
+}
+
+impl SquareComparison {
+	fn empty_or_captureable(&self) -> bool {
+		if let SquareComparison::ColorSame = self {
+			false
+		} else {
+			true
+		}
+	}
+	
+	fn is_empty(&self) -> bool {
+		if let SquareComparison::Empty = self {
+			true
+		} else {
+			false
+		}
+	}
+	
+	fn is_piece(&self) -> bool {
+		if let SquareComparison::Empty = self {
+			false
+		} else {
+			true
+		}
+	}
+	
+	//useless?
+	fn empty_or_same_color(&self) -> bool {
+		if let SquareComparison::ColorOther = self {
+			false
+		} else {
+			true
+		}
+	}
 }
 
 #[derive(Copy, Clone)]
@@ -151,6 +222,11 @@ impl PieceType {
 	}
 }
 
+struct CordOffset {
+	x: i8,
+	y: i8
+}
+
 #[derive(Copy, Clone)]
 struct Cord {
 	x: u8,
@@ -159,11 +235,68 @@ struct Cord {
 
 impl Cord {
 	fn new(x:u8, y:u8) -> Cord {
-		Cord{ x:x, y:y }
+		Cord{ x, y }
 	}
 	
 	fn from_tuple(x: (u8, u8)) -> Cord {
 		Cord{ x:x.0, y:x.1 }
+	}
+	
+	fn offset_xy(&self, x:i8, y:i8) -> Cord {
+		Cord{
+			x:(self.x as i8 + x) as u8,
+			y:(self.y as i8 + y) as u8
+		}
+	}
+	
+	
+	fn offset_x(&self, x:i8) -> Cord {
+		Cord{
+			x:(self.x as i8 + x) as u8,
+			y:self.y
+		}
+	}
+	
+	fn offset_y(&self, y:i8) -> Cord {
+		Cord{
+			x:self.x,
+			y:(self.y as i8 + y) as u8
+		}
+	}
+	
+	fn offset(&self, o: CordOffset) -> Cord {
+		Cord{
+			x:(self.x as i8 + o.x) as u8,
+			y:(self.y as i8 + o.y) as u8
+		}
+	}
+	
+	
+	fn sum(&self, cord: &Cord) -> Cord {
+		Cord{
+			x:self.x + cord.x,
+			y:self.y + cord.y
+		}
+	}
+	
+	fn is_in_range(&self) -> bool {
+		if self.x_is_in_range() {
+			return self.y_is_in_range()
+		}
+		false
+	}
+	
+	fn x_is_in_range(&self) -> bool {
+		if self.x >= 0 {
+			return self.x < 8
+		}
+		false
+	}
+	fn y_is_in_range(&self) -> bool {
+		if self.y >= 0 {
+			return self.y < 8
+		}
+		false
 	}
 }
 
@@ -180,6 +313,12 @@ impl fmt::Display for Cord {
 struct Move {
 	from: Cord,
 	to: Cord
+}
+
+impl Move {
+	fn new(from: Cord, to: Cord) -> Self {
+		Move{from,to}
+	}
 }
 
 impl fmt::Display for Move {
@@ -341,6 +480,170 @@ impl Game {
 			.collect()
 	}
 	*/
+	
+	fn square_at(&self, cord: Cord) -> Square {
+		self.board.0[cord.x as usize][cord.y as usize]
+	}
+	
+	fn get_moves_cord(&self, cord: Cord) -> Vec<Move> {
+		match self.square_at(cord) {
+			Empty => Vec::<Move>::new(),
+			Square::Piece(piece) => {
+				let color = piece.color;
+				match piece.piece_type {
+					Pawn   => self.get_pawn_moves(cord, color),
+					Knight => self.get_knight_moves(cord, color),
+					Bishop => self.get_bishop_moves(cord, color),
+					Rook   => self.get_rook_moves(cord, color),
+					Queen  => self.get_queen_moves(cord, color),
+					King   => self.get_king_moves(cord, color)
+				}
+			}
+		}
+	}
+	
+	fn get_pawn_moves(&self, cord: Cord, color: bool) -> Vec<Move> {
+		let mut v = Vec::<Move>::new();
+		if color {//white
+			let to = cord.offset_y(1);
+			if let Empty = self.square_at(to) {
+				v.push(Move::new(cord,to));
+				
+				if cord.y == 1 {
+					let to = cord.offset_y(2);
+					if let Empty = self.square_at(to) {
+						v.push(Move::new(cord,to));
+					}
+				}
+			}
+			
+			{
+				let to = to.offset_x(1);
+				if let SquareComparison::ColorOther = self.square_at(to).compareColor(color) {
+					v.push(Move::new(cord,to));
+				}
+			}
+			
+			{
+				let to = to.offset_x(-1);
+				if let SquareComparison::ColorOther = self.square_at(to).compareColor(color) {
+					v.push(Move::new(cord,to));
+				}
+			}
+			
+			
+		} else {//black
+			let to = cord.offset_y(-1);
+			if let Empty = self.square_at(to) {
+				v.push(Move::new(cord,to));
+				
+				if cord.y == 6 {
+					let to = cord.offset_y(-2);
+					if let Empty = self.square_at(to) {
+						v.push(Move::new(cord,to));
+					}
+				}
+			}
+			
+			{
+				let to = to.offset_x(1);
+				if let SquareComparison::ColorOther = self.square_at(to).compareColor(color) {
+					v.push(Move::new(cord,to));
+				}
+			}
+			
+			{
+				let to = to.offset_x(-1);
+				if let SquareComparison::ColorOther = self.square_at(to).compareColor(color) {
+					v.push(Move::new(cord,to));
+				}
+			}
+		}
+		v
+	}
+	
+	//maybe optimize with out of bounds check
+	fn get_knight_moves(&self, cord: Cord, color: bool) -> Vec<Move> {
+		let mut v = Vec::<Move>::new();
+		
+		let to = cord.offset_xy(1,2);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		let to = cord.offset_xy(2,1);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		let to = cord.offset_xy(-1,2);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		let to = cord.offset_xy(-2,1);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		let to = cord.offset_xy(1,-2);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		let to = cord.offset_xy(2,-1);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		let to = cord.offset_xy(-1,-2);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		let to = cord.offset_xy(-2,-1);
+		self.add_if_unobstructed_cords(&mut v, &cord, &to, color);
+		
+		v
+	}
+	
+	fn get_bishop_moves(&self, cord: Cord, color: bool) -> Vec<Move> {
+		let mut v = Vec::<Move>::new();
+		v
+	}
+	
+	fn get_rook_moves(&self, cord: Cord, color: bool) -> Vec<Move> {
+		let mut v = Vec::<Move>::new();
+		v
+	}
+	
+	fn get_queen_moves(&self, cord: Cord, color: bool) -> Vec<Move> {
+		let mut v = Vec::<Move>::new();
+		v
+	}
+	
+	fn get_king_moves(&self, cord: Cord, color: bool) -> Vec<Move> {
+		let mut v = Vec::<Move>::new();
+		v
+	}
+	
+	fn add_if_unobstructed( &self, moves: &mut Vec<Move>, m: Move, color: bool) {
+		if m.to.is_in_range() {
+			let square = self.square_at(m.to);
+			match square {
+				Empty => moves.push(m),
+				Square::Piece(piece) => if piece.color != color {
+					moves.push(m);
+				}
+			}
+		}
+	}
+	
+	fn add_if_unobstructed_cords(&self, moves: &mut Vec<Move>, from: &Cord, to: &Cord, color: bool) {
+		if to.is_in_range() {
+			let square = self.square_at(*to);
+			match square {
+				Empty => moves.push(Move::new(*from,*to)),
+				Square::Piece(piece) => if piece.color != color {
+					moves.push(Move::new(*from,*to));
+				}
+			}
+		}
+	}
+	
+	fn is_move_legal(&self, m: &Move) -> bool {
+		false
+	}
+	
+	fn is_in_check(&self) -> bool {
+		false
+	}
 	
 	fn game_state(&self) -> GameState {
 		//todo: add logic to determine win, loss, stalemate
